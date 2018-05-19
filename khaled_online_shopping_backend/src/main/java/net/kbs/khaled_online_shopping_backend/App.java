b74f9181c93876cd31f2425caf60e59a7e7d5879
khaled <kbskhaled@gmail.com>
2018-05-19 20:53:09 +0200

    my project setup is completed successfully

---
+++ khaled_online_shopping_backend/src/main/java/net/kbs/khaled_online_shopping_backend/App.java
@@ -0,0 +1,13 @@
+package net.kbs.khaled_online_shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
